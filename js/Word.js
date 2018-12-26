7184e3d5974fd55f02c400ced166619476841c0f
donjoe <giumontanaro@gmail.com>
2018-12-27 00:54:48 +0100

    Define Word module
    Responsible for getting the word, shuffle it and set it as textContent of game__word container

---
+++ js/Word.js
@@ -0,0 +1,48 @@
+const Word = (function() {
+
+    const DOM = {};
+
+    String.prototype.shuffle = function () {
+        let arr = this.split(""); 
+        let length = arr.length;
+    
+        for(let i = length - 1; i > 0; i--) { 
+            let random = Math.floor(Math.random() * (i + 1)); 
+            
+            let tmp = arr[i]; 
+            arr[i] = arr[random]; 
+            arr[random] = tmp;
+        }
+        return arr.join("");
+    };
+
+    function cacheDOM() {
+        DOM.wordContainer = document.querySelector('.game__word');
+    }
+
+    function getWords() {
+         fetch('../assets/google10000English.txt')
+        .then(res => res.text())
+        .then(getRandomWord)
+    }
+
+    function getRandomWord(text) {
+        const arrOfWords = text.split('\n');
+        const filteredWords = arrOfWords.filter(w => w.length > 3);
+        const random = Math.floor(Math.random() * filteredWords.length);
+        publicAPI.randomWord = filteredWords[random]; //when the async job is done, set word publicly
+        DOM.wordContainer.textContent = filteredWords[random].shuffle();
+    }
+
+    function init() {
+        cacheDOM()
+        getWords()             
+    }
+   
+    const publicAPI = {
+        init: init
+    };
+
+    return publicAPI;
+    
+})();
